2047848be6980b174c84b32b2ecf4689f8222d42
Gabriella <gabriella.inacio@sptech.school>
2023-11-20 23:05:10 -0300

    atualizacoes da captura kotlin - ainda n/funcional

---
+++ Meu_Individual/power-tech/target/classes/CapturaProcessos.kts
@@ -0,0 +1,70 @@
+class CapturaProcessos{
+
+    fun pythonProc(){
+
+        val codigoPy= """
+import psutil
+import mysql.connector
+import time
+from datetime import datetime
+
+try:
+        #Estabelece uma conexao com o banco de dados
+        conexao = mysql.connector.connect(
+        host='localhost',
+        user='Power',
+        password='urubu100',
+        database='powertechsolutions'
+        )
+            
+        # Criar um cursor, que será utilzado para realizar os comandos mysql 
+        cursor = conexao.cursor()
+
+        # Lista de processos do psutil
+        lista_processos = psutil.process_iter()
+
+        # Contador para de inserts
+        contador_inserts = 0
+
+        # Contador para delimitar o número de capturas
+        contador_processos = 50
+
+        print("Iniciando capturas")
+
+        # Iterar sobre os processos e inserir no banco de dados
+        for process in lista_processos:
+                process_info = process.as_dict(attrs=['name', 'cpu_times'])
+                nome = process_info['name']
+                cpu_user = process_info['cpu_times'].user
+                data_hora_captura = datetime.now()
+
+                if cpu_user > 0:
+                    cursor.execute('''
+                    INSERT INTO processos (nome, tempo_user, dthora_captura)
+                    VALUES (%s, %s, %s)
+                    ''', (nome, cpu_user, data_hora_captura))
+                    contador_inserts+=1
+
+                if contador_inserts == contador_processos:
+                    print("...")
+                    print("Capturas realizadas com sucesso!")
+                    break
+                    
+
+finally:
+        #Commit das alterações e fechar a conexão com o banco de dados
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+
+        """.trimIndent()
+    }
+
+    val nomeArquivoPyDefault = "CodigoPythonProc.py"
+
+    File(nomeArquivoPyDefault).writeText(codigoPython)
+    Runtime.getRuntime().exec("python3 $nomeArquivoPyDefault")
+
+    println("Processos capturados!")
+
+}
--- capturas/captura_py.py
+++ capturas/captura_py.py
@@ -1,39 +1,52 @@
-import psutil
 import mysql.connector
+import psutil
+import time
+from datetime import datetime
 
-#Estabelece uma conexao com o banco de dados
-conexao = mysql.connector.connect(
-host='localhost',
-user='aluno',
-password='sptech',
-database='powertechsolutions'
-)
-    
-# Criar um cursor, que será utilzado para realizar os comandos mysql 
-cursor = conexao.cursor()
+try:
+    # Estabelece uma conexão com o banco de dados
+    conexao = mysql.connector.connect(
+        host='localhost',
+        user='Power',
+        password='urubu100',
+        database='PowerTechSolutions'
+    )
+            
+    # Criar um cursor, que será utilizado para realizar os comandos MySQL 
+    cursor = conexao.cursor()
 
-# Lista de processos do psutil
-lista_processos = psutil.process_iter()
+    # Lista de processos do psutil
+    lista_processos = psutil.process_iter()
 
-# Contador para delimitar o número de capturas
-contador_processos = 300
+    # Contador para de inserts
+    contador_inserts = 0
 
-# Iterar sobre os processos e inserir no banco de dados
-for process in lista_processos:
+    # Contador para delimitar o número de capturas
+    contador_processos = 50
+
+    print("Iniciando capturas")
+
+    # Iterar sobre os processos e inserir no banco de dados
+    for process in lista_processos:
         process_info = process.as_dict(attrs=['name', 'cpu_times'])
         nome = process_info['name']
         cpu_user = process_info['cpu_times'].user
+        data_hora_captura = datetime.now()
 
         if cpu_user > 0:
             cursor.execute('''
-            INSERT INTO processos (nome, tempo_user)
-            VALUES (%s, %s)
-            ''', (nome, cpu_user))
+                INSERT INTO processos (nome, tempo_user, dthora_captura)
+                VALUES (%s, %s, %s)
+            ''', (nome, cpu_user, data_hora_captura))
+            contador_inserts += 1
 
-        if process == contador_processos:
+        if contador_inserts == contador_processos:
+            print("...")
+            print("Capturas realizadas com sucesso!")
             break
 
-#Commit das alterações e fechar a conexão com o banco de dados
-conexao.commit()
-cursor.close()
-conexao.close()
+finally:
+    # Commit das alterações e fechar a conexão com o banco de dados
+    conexao.commit()
+    cursor.close()
+    conexao.close()
