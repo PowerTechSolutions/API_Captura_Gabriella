9f99b69dd1fdf8398bb5fe452516c20fe8ceae54
Gabriella <gabriella.inacio@sptech.school>
2023-12-05 19:58:25 -0300

    Ajeitando

--- capturas_v1/KotlinComPython_Gabriella/src/main/kotlin/Conexao.kt
+++ capturas_v1/KotlinComPython_Gabriella/src/main/kotlin/Conexao.kt
@@ -6,8 +6,8 @@ open class Conexao {
         val dataSource = BasicDataSource()
         dataSource.driverClassName = "com.mysql.cj.jdbc.Driver"
         dataSource.url = "jdbc:mysql://localhost:3306/PowerTechSolutions?serverTimezone=UTC&useSSL=false"
-        dataSource.username = "aluno"
-        dataSource.password = "sptech"
+        dataSource.username = "Power"
+        dataSource.password = "urubu100"
         return JdbcTemplate(dataSource)
     }
 }
--- capturas_v1/KotlinComPython_Gabriella/src/main/kotlin/app/Main.kt
+++ capturas_v1/KotlinComPython_Gabriella/src/main/kotlin/app/Main.kt
@@ -17,17 +17,17 @@ open class Main {
             println("Bem-vindo a PowerTech. Por favor, faca o login para usar nosso sistema.\r\n")
             println("Insira seu email:")
 
-            val senhaInserida = Scanner(System.`in`)
-            val senha = senhaInserida.next().toString()
+            val emailInserido = Scanner(System.`in`)
+            val email = emailInserido.next().toString()
 
             repositorio.iniciarJdbc()
 
-            usuario.Email = senha
+            usuario.Email = email
 
             if (repositorio.validar(usuario.Email)) {
                 usuario.IDUsuario = repositorio.pegarId(usuario.Email)
                 val maquinas = repositorio.resgatarMaquinas(usuario.IDUsuario)
-                println("Qual a numeracao da maquina em que esta instalando o servico? $maquinas\"\r\n")
+                println("Qual a numeracao da maquina em que esta instalando o servico? $maquinas \r\n")
 
                 val maquinaEscolhida = Scanner(System.`in`)
                 maquina.IDMaquina = maquinaEscolhida.nextInt()
